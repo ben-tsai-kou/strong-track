617f478a42402841e11d5903e3ce5f5b3dc9964a
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-01-16 17:40:29 +0900

    Add ESLint and Prettier configuration files; update package dependencies and scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': '@typescript-eslint/eslint-plugin',
+            prettier: 'eslint-plugin-prettier',
+        },
+        rules: {
+            'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        },
+    },
+    ...compat.extends('next/core-web-vitals'),
+    ...compat.extends('next/typescript'),
+    prettier,
 ];
 
 export default eslintConfig;
